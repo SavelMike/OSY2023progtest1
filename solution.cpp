a1f00cf92d8cac142383869f98cd7860e417a0a5
Mike <savelievmv@gmail.com>
2023-03-20 01:56:52 +0100

    Struct CProblemPackExt is added. It includes AProblemPack, std::vector<AProblemPack> and semaphore

--- solution.cpp
+++ solution.cpp
@@ -58,26 +58,71 @@ private:
 	std::vector<pthread_t> m_Thread_ids;
 };
 
+struct CProblemPackExt {
+	AProblemPack m_pack;
+	std::vector<AProblemPack> m_list;
+	sem_t m_semaphore;
+};
+
+using AProblemPackExt = std::shared_ptr<struct CProblemPackExt>;
+
+// List of AProblemPack to be processed
+std::vector<struct CProblemPackExt> g_Input_vec;
+// Global mutex to protect g_Input_vec
+pthread_mutex_t g_Input_vec_mutex = PTHREAD_MUTEX_INITIALIZER;
+// Cond variable
+pthread_cond_t g_Input_vec_cv = PTHREAD_COND_INITIALIZER;
+
 // TODO: COptimizer implementation goes here
 void *worker_thread(void * arg) {
 	CProgtestSolver solver = createProgtestSolver();
 	while(true) {
 		// wait until there is problem_pack
+		pthread_mutex_lock(&g_Input_vec_mutex);
+		while (g_Input_vec.size() == 0) {
+			pthread_cond_wait(&g_Input_vec_cv, &g_Input_vec_mutex);
+		}
+		APoblemPack work_pack = g_Input_vec.begin();
+		g_Input_vec.erase(g_Input_vec.begin());
+		pthread_mutex_unlock(&g_Input_vec_mutex);
 		// add CProblems to solver from problem_pack
-		// start solving
+		for (auto problem :  work_pack.m_Problems) {
+			if (solver.addProblem(problem)) {
+				continue;
+			}
+			solver.solve();
+			if (!solver.addProblem(problem)) {
+				throw std::logic_error("Error in solver");
+			}
+		}
+		//  solve for left problems
 		solver.solve();
 		// return solved pack to company
+		
 	}
 }
 
 void *communication_get_thread(void* arg) {
 	ACompany company = arg;
+	std::vector<AProblemPack>  output_vec = std::make_shared<std::vector<AProblemPack>> ();
+	
 	while (true) {
 		AProblemPack pack = company.waitForPack();
 		if (pack == nullptr) {
 			break;
 		}
+		AProblemPackExt pack_ext = std::make_shared<CProblemPackExt> ();
+		
+		AProblemPack res = std::make_shared<CProblemPack> ();
+
+		pack_ext.m_pack = pack;
+		pack_ext.m_list = output_vec;
+		sem_init(&pack_ext.m_semaphore);
 		// Add pack to shared input list to the end
+		mutex_lock(&g_Input_vec_lock);
+		g_Input_vec.push_back(pack_ext);
+		pthread_cond_signal(&g_Input_vec_cv);
+		mutex_unlock(&g_Input_vec_lock);
 	}
 }
 
